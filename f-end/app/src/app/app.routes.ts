305d14cafe419d8b26c2f5a5f797be353011ede8
Arba Marco <arba.marco@iisgalvanimi.edu.it>
2024-11-26 13:07:34 +0000

    prova 2

---
+++ f-end/app/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+
+
+export const routes: Routes = [];
